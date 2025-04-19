1e40218d8b1d63ef04919c574affb2381186224d
Joel Jolly <128782382+withinJoel@users.noreply.github.com>
2025-04-19 17:30:51 +0530

    Update Header.js

--- Header/Header.js
+++ Header/Header.js
@@ -14,8 +14,8 @@ document.writeln(`
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<meta name="description" content="${appdescription}" />
-	<meta name="keywords" content="${appkeywords}" />
+	<meta name="description" content="${appDescription}" />
+	<meta name="keywords" content="${appKeywords}" />
 	<meta name="author" content="${appAuthor}" />
 	<link rel="icon" href="${appLogo}">
 	<link rel="stylesheet" href="Styles/Styles.css">
@@ -23,16 +23,16 @@ document.writeln(`
 	<meta name="HandheldFriendly" content="true">
 	<meta name="distribution" content="global">
 	<meta name="rating" content="general">
-	<meta name="twitter:title" content="${appname}">
-	<meta name="twitter:description" content="${appdescription}">
-	<meta name="twitter:image" content="${applogo}">
+	<meta name="twitter:title" content="${appName}">
+	<meta name="twitter:description" content="${appDescription}">
+	<meta name="twitter:image" content="${appLogo}">
 	<meta name="twitter:site" content="${authorSocialHandle}">
 	<meta name="language" content="English">
-	<title>${appname}</title>
+	<title>${appName}</title>
 	<link rel="manifest" href="manifest.json">
-	<meta property="og:title" content="${appname}" />
-	<meta property="og:description" content="${appdescription}" />
-	<meta property="og:image" content="${applogo}" />
+	<meta property="og:title" content="${appName}" />
+	<meta property="og:description" content="${appDescription}" />
+	<meta property="og:image" content="${appLogo}" />
 	<meta property="og:type" content="website" />
 </head>
 `);
