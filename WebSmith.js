49916d09a17d7076d15220e0a14f6b222cfbffc8
Joel Jolly <128782382+withinJoel@users.noreply.github.com>
2025-08-17 17:21:40 +0530

    Update WebSmith.js

--- WebSmith.js
+++ WebSmith.js
@@ -1,9 +1,9 @@
-// Head
-createScript("https://cdn.jsdelivr.net/gh/withinjoel/WebSmith@main/Head/Head.js");
-
 // Loader
 function createScript(src) {
     const script = document.createElement('script');
     script.src = src;
     document.head.appendChild(script);
 }
+
+// Head
+createScript("https://cdn.jsdelivr.net/gh/withinjoel/WebSmith@main/Head/Head.js");
